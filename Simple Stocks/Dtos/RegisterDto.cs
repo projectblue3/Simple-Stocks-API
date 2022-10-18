b6898d466f4db976d73d2b095d8b73f13c5615cc
Project Blue <projectblue3@gmail.com>
2022-10-18 12:56:47 -0400

    Fixed regex for password rules

--- Simple Stocks/Dtos/RegisterDto.cs
+++ Simple Stocks/Dtos/RegisterDto.cs
@@ -22,7 +22,7 @@ namespace Simple_Stocks.Dtos
 
         [Required]
         [MinLength(8)]
-        //[RegularExpression(@"^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])$", ErrorMessage = "Password must include an uppercase letter, lowercase letter, special charactor (!@#$&*), and a number")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$", ErrorMessage = "Password must include an uppercase letter, lowercase letter, special charactor, and a number")]
         public string Password { get; set; } = string.Empty;
 
         [Required]
